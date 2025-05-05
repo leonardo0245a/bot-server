ccba1537680dc741f52b3bf9f9acac9b7fcc45ce
Leonardo <leonardo0245a@email.com>
2025-05-05 06:59:32 +0200

    Fix: render deployment (disable debug/reloader)

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def ping():
     return jsonify({"status": "online"})
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
